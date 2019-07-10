4bee18fa8641edbce50ce51297e77a34d3bb5b2c
Siny <51217784+sinyozz@users.noreply.github.com>
2019-07-10 00:14:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(() =>  {
+
+("#box").on('oneclick', e => {
+    alert ("Box was clicked")
+}
+)
+ console.log("addclass,removeclass");
+
+})
+
+
+// //var acc = document.getElementsByClassName("accordion");
+// // var i;//
+
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", function() {
+
+//         this.classList.toggle("active"); 
+    
+//         var panel = this.nextElementSibling;
+//         if (panel.style.display === "block") {
+//           panel.style.display = "none";
+//         } else {
+//           panel.style.display = "block";
+//         }
+//       });
+////
